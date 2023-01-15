a21d9f93c85dbb46019730104057933c86ad3c53
DizzzM <76074137+DizzzM@users.noreply.github.com>
2023-01-15 15:18:35 +0200

    Update get_from_musixmatch.py

--- website/api_connections/get_from_musixmatch.py
+++ website/api_connections/get_from_musixmatch.py
@@ -4,7 +4,7 @@ from parameters import *
 
 
 def get_lyrics_by_isrc(isrc):
-    request = requests.get(f'https://api.musixmatch.com/ws/1.1/track.get?track_isrc={isrc}&apikey={MUSIXMATCH_KEY}').json()
+    request = requests.get(f'https://api.musixmatch.com/ws/1.1/track.get?track_isrc={isrc}&apikey={os.getenv('MUSIXMATCH_KEY')}').json()
     values = request['message']['body']
     lyrics = values['track']['track_share_url'] if len(values) > 0 else None
     return lyrics
